81a8a6a3b050793f8a24155e91eb1212d75ea02d
Diogo <you@example.com>
2021-03-24 16:53:37 -0300

    Aula 01-Atividade-02 term

---
+++ fogefoge.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){    
+    // armazenando na posicao 0x0
+    char mapa[5][10];
+
+    mapa[0][0] = '|';
+    mapa [4][9] = '@';
+
+    // imprimindo a posição 2x3
+    printf("%c""%c\n", mapa[0][0], mapa[4][9]);
+
+
+}
