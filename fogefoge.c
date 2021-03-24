5dfde25b11df50e684872aafc1e7c1a589956dc9
Diogo <you@example.com>
2021-03-24 17:08:24 -0300

    Aula 01-Atividade-04 term

--- fogefoge.c
+++ fogefoge.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){    
-    // armazenando na posicao 0x0
-    char mapa[5][10];
+    // matriz de 5 x 10
+    char mapa[5][10+1];
 
-    mapa[0][0] = '|';
-    mapa [4][9] = '@';
+    FILE* f;
+    f = fopen("mapa.txt", "r");
+    if(f == 0){
+        printf("Erro na leitura do mapa\n");
+        exit(1);
+    }
 
-    // imprimindo a posição 2x3
-    printf("%c""%c\n", mapa[0][0], mapa[4][9]);
+    for(int i=0; i<5; i++) {
+        fscanf(f, "%s\n", mapa[i]);
+    }
 
+    for(int i=0; i<5; i++) {
+        printf("%s\n", mapa[i]);
+    }
+
+    fclose(f);
 
 }
