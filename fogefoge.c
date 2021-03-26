aa787b79e3bfe1f67383a2637bcc774f161a197d
Diogo0602x <diogoteixeira0602@gmail.com>
2021-03-26 16:26:39 -0300

    Aula 01-Atividade-09 term

--- fogefoge.c
+++ fogefoge.c
@@ -1,47 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "fogefoge.h"
 
 char** mapa;
 int linhas;
 int colunas;
 
-void liberamapa() {
-    for(int i = 0; i < linhas; i++){
-        free(mapa[i]);
-    }
-    free(mapa);
+void lemapa() {
+	FILE* f;
+	f = fopen("mapa.txt", "r");
+	if(f == 0) {
+		printf("Erro na leitura do mapa");
+		exit(1);
+	}
+
+	fscanf(f, "%d %d", &linhas, &colunas);
+	alocamapa();
+	
+	for(int i = 0; i < 5; i++) {
+		fscanf(f, "%s", mapa[i]);
+	}
+
+	fclose(f);
 }
 
-int main() {    
+void alocamapa() {
+	mapa = malloc(sizeof(char*) * linhas);
 
-    FILE* f;
-    f = fopen("mapa.txt", "r");
-    if(f == 0) {
-        printf("Erro na leitura do mapa\n");
-        exit(1);
-    }
+	for(int i = 0; i < linhas; i++) {
+		mapa[i] = malloc(sizeof(char) * colunas + 1);
+	}
+}
 
-    fscanf(f, "%d %d", &linhas, &colunas);
-    printf("linhas %d colunas %d\n", linhas, colunas);
+void liberamapa() {
+	for(int i = 0; i < linhas; i++) {
+		free(mapa[i]);
+	}
 
-    mapa = malloc(sizeof(char*) * linhas);
-    for(int i = 0; i < linhas; i++) {
-        mapa[i] = malloc(sizeof(char) * (colunas+1));
-    }
+	free(mapa);
+}
 
-    for(int i=0; i<5; i++) {
-        fscanf(f, "%s", mapa[i]);
-    }
+int main() {
+	
+	lemapa();
 
-    for(int i=0; i<5; i++) {
-        printf("%s\n", mapa[i]);
-    }
+	for(int i = 0; i < linhas; i++) {
+		printf("%s\n", mapa[i]);
+	}
 
-    fclose(f);
-
-    for(int i = 0; i < linhas; i++) {
-        free(mapa[i]);
-    }
-    free(mapa);
-    
+	liberamapa();
 }
---
+++ fogefoge.h
@@ -0,0 +1,3 @@
+void alocamapa();
+void lemapa();
+void liberamapa();
