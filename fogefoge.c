48f6e0525bf23a3e492ea7e7f3783e11ee810943
Diogo0602x <diogoteixeira0602@gmail.com>
2021-03-31 10:20:06 -0300

    Aula-02-Atividade-01 term

--- fogefoge.c
+++ fogefoge.c
@@ -40,13 +40,61 @@ void liberamapa() {
 	free(mapa);
 }
 
+int acabou() {
+	return 0;
+}
+
+void move(char direcao) {
+	int x;
+	int y;
+
+	for(int i = 0; i < linhas; i++) {
+		for(int j = 0; j < colunas; j++) {
+			if(mapa[i][j] == '@') {
+				x = i;
+				y = j;
+				break;
+			}
+		}
+	} 
+
+	switch(direcao) {
+		case 'a':
+			mapa[x][y-1] = '@';
+			break;
+		case 'w':
+			mapa[x-1][y] = '@';
+			break;
+		case 's':
+			mapa[x+1][y] = '@';
+			break;
+		case 'd':
+			mapa[x][y+1] = '@';
+			break;
+	}
+
+	mapa[x][y] = '.';
+}
+
+void imprimemapa() {
+	for(int i = 0; i < linhas; i++) {
+		printf("%s\n", mapa[i]);
+	}
+}
+
 int main() {
 	
 	lemapa();
 
-	for(int i = 0; i < linhas; i++) {
-		printf("%s\n", mapa[i]);
-	}
+	do {
+		imprimemapa();
+
+		char comando;
+		scanf(" %c", &comando);
+
+		move(comando);
+
+	} while (!acabou());
 
 	liberamapa();
 }
